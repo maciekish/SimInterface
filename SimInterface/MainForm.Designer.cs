b9fa9cf235c4ec4ee545ddb06c1b2a132653b410
Maciej Swic <m@maciekish.com>
2014-03-07 19:42:13 +0100

    Added a basic FSX project. Doesnt work with PMDG though.

---
+++ SimInterface/MainForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SimInterface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chk_efis_wxr = new System.Windows.Forms.CheckBox();
+            this.chk_efis_terr = new System.Windows.Forms.CheckBox();
+            this.chk_efis_data = new System.Windows.Forms.CheckBox();
+            this.chk_efis_arpt = new System.Windows.Forms.CheckBox();
+            this.chk_efis_sta = new System.Windows.Forms.CheckBox();
+            this.chk_efis_wpt = new System.Windows.Forms.CheckBox();
+            this.chk_efis_pos = new System.Windows.Forms.CheckBox();
+            this.chk_mcp_cmd_a = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chk_efis_wxr
+            // 
+            this.chk_efis_wxr.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_wxr.Location = new System.Drawing.Point(12, 63);
+            this.chk_efis_wxr.Name = "chk_efis_wxr";
+            this.chk_efis_wxr.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_wxr.TabIndex = 7;
+            this.chk_efis_wxr.Text = "WXR";
+            this.chk_efis_wxr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_wxr.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_terr
+            // 
+            this.chk_efis_terr.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_terr.Location = new System.Drawing.Point(63, 63);
+            this.chk_efis_terr.Name = "chk_efis_terr";
+            this.chk_efis_terr.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_terr.TabIndex = 8;
+            this.chk_efis_terr.Text = "TERR";
+            this.chk_efis_terr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_terr.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_data
+            // 
+            this.chk_efis_data.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_data.Location = new System.Drawing.Point(165, 63);
+            this.chk_efis_data.Name = "chk_efis_data";
+            this.chk_efis_data.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_data.TabIndex = 10;
+            this.chk_efis_data.Text = "DATA";
+            this.chk_efis_data.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_data.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_arpt
+            // 
+            this.chk_efis_arpt.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_arpt.Location = new System.Drawing.Point(114, 63);
+            this.chk_efis_arpt.Name = "chk_efis_arpt";
+            this.chk_efis_arpt.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_arpt.TabIndex = 9;
+            this.chk_efis_arpt.Text = "ARPT";
+            this.chk_efis_arpt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_arpt.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_sta
+            // 
+            this.chk_efis_sta.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_sta.Location = new System.Drawing.Point(318, 63);
+            this.chk_efis_sta.Name = "chk_efis_sta";
+            this.chk_efis_sta.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_sta.TabIndex = 13;
+            this.chk_efis_sta.Text = "STA";
+            this.chk_efis_sta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_sta.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_wpt
+            // 
+            this.chk_efis_wpt.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_wpt.Location = new System.Drawing.Point(267, 63);
+            this.chk_efis_wpt.Name = "chk_efis_wpt";
+            this.chk_efis_wpt.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_wpt.TabIndex = 12;
+            this.chk_efis_wpt.Text = "WPT";
+            this.chk_efis_wpt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_wpt.UseVisualStyleBackColor = true;
+            // 
+            // chk_efis_pos
+            // 
+            this.chk_efis_pos.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_efis_pos.Location = new System.Drawing.Point(216, 63);
+            this.chk_efis_pos.Name = "chk_efis_pos";
+            this.chk_efis_pos.Size = new System.Drawing.Size(45, 45);
+            this.chk_efis_pos.TabIndex = 11;
+            this.chk_efis_pos.Text = "POS";
+            this.chk_efis_pos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_efis_pos.UseVisualStyleBackColor = true;
+            // 
+            // chk_mcp_cmd_a
+            // 
+            this.chk_mcp_cmd_a.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_mcp_cmd_a.Location = new System.Drawing.Point(318, 12);
+            this.chk_mcp_cmd_a.Name = "chk_mcp_cmd_a";
+            this.chk_mcp_cmd_a.Size = new System.Drawing.Size(45, 45);
+            this.chk_mcp_cmd_a.TabIndex = 14;
+            this.chk_mcp_cmd_a.Text = "CMD A";
+            this.chk_mcp_cmd_a.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chk_mcp_cmd_a.UseVisualStyleBackColor = true;
+            this.chk_mcp_cmd_a.CheckedChanged += new System.EventHandler(this.chk_mcp_cmd_a_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 118);
+            this.Controls.Add(this.chk_mcp_cmd_a);
+            this.Controls.Add(this.chk_efis_sta);
+            this.Controls.Add(this.chk_efis_wpt);
+            this.Controls.Add(this.chk_efis_pos);
+            this.Controls.Add(this.chk_efis_data);
+            this.Controls.Add(this.chk_efis_arpt);
+            this.Controls.Add(this.chk_efis_terr);
+            this.Controls.Add(this.chk_efis_wxr);
+            this.Name = "MainForm";
+            this.Text = "SimInterface";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chk_efis_wxr;
+        private System.Windows.Forms.CheckBox chk_efis_terr;
+        private System.Windows.Forms.CheckBox chk_efis_data;
+        private System.Windows.Forms.CheckBox chk_efis_arpt;
+        private System.Windows.Forms.CheckBox chk_efis_sta;
+        private System.Windows.Forms.CheckBox chk_efis_wpt;
+        private System.Windows.Forms.CheckBox chk_efis_pos;
+        private System.Windows.Forms.CheckBox chk_mcp_cmd_a;
+    }
+}
+
---
+++ SimInterface/MainForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+// Add these two statements to all SimConnect clients
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+namespace SimInterface
+{
+    public partial class MainForm : Form
+    {
+        // User-defined win32 event
+        const int WM_USER_SIMCONNECT = 0x0402;
+
+        // SimConnect object
+        SimConnect simconnect = null;
+
+        enum EVENTS
+        {
+            AP_MASTER,
+            PITOT_TOGGLE,
+            FLAPS_INC,
+            FLAPS_DEC,
+            FLAPS_UP,
+            FLAPS_DOWN,
+        };
+
+        enum DEFINITION
+        {
+            MCP
+        }
+
+        enum DATA_REQUESTS
+        {
+            REQUEST_MCP,
+        };
+
+        struct MCP
+        {
+            public bool ap_master;
+        };
+
+        enum NOTIFICATION_GROUPS
+        {
+            GROUP0,
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //Handle incoming messages
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_USER_SIMCONNECT)
+            {
+                if (simconnect != null)
+                {
+                    simconnect.ReceiveMessage();
+                }
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        private void closeConnection()
+        {
+            if (simconnect != null)
+            {
+                simconnect.Dispose();
+                simconnect = null;
+                Debug.Write("Disconnected");
+            }
+        }
+
+        // Set up all the SimConnect related event handlers
+        private void initClientEvent()
+        {
+            try
+            {
+                // listen to connect and quit msgs
+                simconnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(simconnect_OnRecvOpen);
+                simconnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(simconnect_OnRecvQuit);
+                simconnect.OnRecvException += new SimConnect.RecvExceptionEventHandler(simconnect_OnRecvException);
+                simconnect.OnRecvEvent += new SimConnect.RecvEventEventHandler(simconnect_OnRecvEvent);
+                
+                //Subscribe to events. Used to trigger data requests.
+                simconnect.MapClientEventToSimEvent(EVENTS.AP_MASTER, "AP_MASTER");
+                simconnect.AddClientEventToNotificationGroup(NOTIFICATION_GROUPS.GROUP0, EVENTS.AP_MASTER, false);
+
+                //Data request definitions. These are used when when we receive events.
+                simconnect.AddToDataDefinition(DEFINITION.MCP, "Autopilot Master", "Boolean", SIMCONNECT_DATATYPE.INT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                
+                simconnect.RegisterDataDefineStruct<MCP>(DEFINITION.MCP);
+                
+                simconnect.OnRecvSimobjectDataBytype += new SimConnect.RecvSimobjectDataBytypeEventHandler(simconnect_OnRecvSimobjectDataBytype);
+
+                // set the group priority
+                simconnect.SetNotificationGroupPriority(NOTIFICATION_GROUPS.GROUP0, SimConnect.SIMCONNECT_GROUP_PRIORITY_HIGHEST);
+            }
+            catch (COMException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        void simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+            Debug.WriteLine("Connected to FSX");
+        }
+
+        // The case where the user closes FSX
+        void simconnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            Debug.WriteLine("FSX has exited");
+            closeConnection();
+        }
+
+        void simconnect_OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
+        {
+            Debug.WriteLine("Exception received: " + data.dwException);
+        }
+
+        //Event received. Should trigger a request for the actual data.
+        void simconnect_OnRecvEvent(SimConnect sender, SIMCONNECT_RECV_EVENT recEvent)
+        {
+            switch (recEvent.uEventID)
+            {
+                case (uint)EVENTS.AP_MASTER:
+                    simconnect.RequestDataOnSimObjectType(DATA_REQUESTS.REQUEST_MCP, DEFINITION.MCP, 0, SIMCONNECT_SIMOBJECT_TYPE.USER);
+                    break;
+            }
+        }
+
+        //Data received in response to a request being sent in response to an event being received. Phew.
+        void simconnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+            switch ((DATA_REQUESTS)data.dwRequestID)
+            {
+                case DATA_REQUESTS.REQUEST_MCP:
+                    MCP mcp = (MCP)data.dwData[0];
+
+                    chk_mcp_cmd_a.CheckedChanged -= chk_mcp_cmd_a_CheckedChanged;
+                    chk_mcp_cmd_a.Checked = mcp.ap_master;
+                    chk_mcp_cmd_a.CheckedChanged += chk_mcp_cmd_a_CheckedChanged;
+                    break;
+                default:
+                    Debug.WriteLine("Unknown request ID: " + data.dwRequestID);
+                    break;
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closeConnection();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (simconnect == null)
+            {
+                try
+                {
+                    simconnect = new SimConnect("Managed Client Events", this.Handle, WM_USER_SIMCONNECT, null, 0);
+                    initClientEvent();
+                }
+                catch (COMException ex)
+                {
+                    Debug.WriteLine("Unable to connect to FSX " + ex.Message);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Error - try again");
+                closeConnection();
+            }
+        }
+
+        private void chk_mcp_cmd_a_CheckedChanged(object sender, EventArgs e)
+        {
+            simconnect.TransmitClientEvent(SimConnect.SIMCONNECT_OBJECT_ID_USER, EVENTS.AP_MASTER, 1, NOTIFICATION_GROUPS.GROUP0, SIMCONNECT_EVENT_FLAG.DEFAULT);
+        }
+    }
+}
